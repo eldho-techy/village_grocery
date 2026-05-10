b8807556b831f0cc457012babb2edc37267f488c
Eldhose Pokkathayil Jose <eldhop99@gmail.com>
2026-05-10 10:00:23 +0200

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './assets/css/style.css'
+import Home from './pages/Home'
+
+function App() {
+  
+
+  return (
+    <Home/>
+  )
+}
+
+export default App
